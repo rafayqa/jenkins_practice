521b04f4f20fc6fa004783d525d1ab7401511b4c
rafayqa <m.rafay@venre.org>
2026-01-09 16:55:38 +0500

    Intentionally adding a failing test

--- login.test.js
+++ login.test.js
@@ -8,3 +8,7 @@ test('valid credentials redirects to Dashboard', () => {
 test('invalid credentials returns error', () => {
   expect(login('user', 'wrongpass')).toBe('Invalid credentials');
 });
+
+test('Intentional failure to test Jenkins HTML report', () => {
+  expect(1 + 1).toBe(3); 
+});
